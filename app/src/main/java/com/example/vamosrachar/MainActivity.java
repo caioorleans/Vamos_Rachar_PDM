46507eca3aa4b17f4eeb6869d6a20ac87587b44b
caioorleans <caioorleansdm@gmail.com>
2021-06-30 11:50:30 -0300

    Adicionado botão de share e tts

--- app/src/main/java/com/example/vamosrachar/MainActivity.java
+++ app/src/main/java/com/example/vamosrachar/MainActivity.java
@@ -2,20 +2,28 @@ package com.example.vamosrachar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnClickListener, TextToSpeech.OnInitListener {
 
     EditText etValor;
     EditText etQPessoas;
     TextView tvResultado;
+    FloatingActionButton share, tts;
+    TextToSpeech ttsPlayer;
 
     double valor1 = 0;
     int valor2 = 2;
@@ -36,6 +44,38 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
         etValor.addTextChangedListener(this);
         etQPessoas.addTextChangedListener(this);
+
+        share = (FloatingActionButton) findViewById(R.id.floatingActionButtonsCompartilhar);
+        tts = (FloatingActionButton) findViewById(R.id.floatingActionButtonTTS);
+
+        share.setOnClickListener(this);
+        tts.setOnClickListener(this);
+
+        Intent checkTTSIntent = new Intent();
+        checkTTSIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
+        startActivityForResult(checkTTSIntent, 1122);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 1122){
+            ttsPlayer = new TextToSpeech(this, this);
+        }
+        else{
+            Intent installTTSIntent = new Intent();
+            installTTSIntent.setAction(TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
+            startActivity(installTTSIntent);
+        }
+    }
+
+    @Override
+    public void onInit(int initStatus){
+        if(initStatus == TextToSpeech.SUCCESS){
+            Toast.makeText(this, "tts ativado...", Toast.LENGTH_LONG).show();
+        }
+        else if(initStatus == TextToSpeech.ERROR){
+            Toast.makeText(this, "tts não habilitado...", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -60,5 +100,20 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         }
     }
 
+
+    @Override
+    public void onClick(View v) {
+        if(v == share){
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT,"A conta dividida por pessoa deu " + tvResultado.getText().toString());
+            startActivity(intent);
+        }
+        if(v == tts){
+            if(tts!=null){
+                ttsPlayer.speak("O valor por pessoa é de " + tvResultado.getText().toString(), TextToSpeech.QUEUE_FLUSH,null,"ID1");
+            }
+        }
+    }
 
 }
